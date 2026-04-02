9a1c5bcea3f3b125edb40fc2ef823bd8031244bb
vinh <vvinh4976@gmail.com>
2026-04-02 12:29:52 +0700

    fxi

--- MainFrame.java
+++ MainFrame.java
@@ -1,38 +1,37 @@
 package view;
-import dao.TransactionDAO;
-import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import controller.TransactionController;
 import model.Transaction;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
-    private DefaultTableModel model;
     private JTable table;
-    private TransactionDAO dao = new TransactionDAO(); // Gọi phần của Hiền
+    private DefaultTableModel tableModel;
+    private TransactionController controller = new TransactionController();
 
     public MainFrame() {
-        setTitle("QUẢN LÝ THU CHI - NHÓM VINH - Ý - HIỀN");
+        // Thiết kế giao diện tại đây
+        setTitle("Quản Lý Thu Chi - Nhóm Vinh Ý Hiền");
         setSize(800, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout(10, 10));
-
-        // Bảng hiển thị
-        model = new DefaultTableModel(new String[]{"ID", "Tiền", "Loại", "Ghi chú", "Ngày"}, 0);
-        table = new JTable(model);
+        
+        tableModel = new DefaultTableModel(new String[]{"ID", "Số Tiền", "Loại", "Ghi Chú", "Ngày"}, 0);
+        table = new JTable(tableModel);
         add(new JScrollPane(table), BorderLayout.CENTER);
 
-        loadData();
-        setVisible(true);
+        loadDataToTable();
     }
 
-    public void loadData() {
-        model.setRowCount(0);
-        for (Transaction t : dao.getAll()) { // Dùng đối tượng của Ý và hàm của Hiền
-            model.addRow(new Object[]{t.getId(), t.getAmount(), t.getCategory(), t.getNote(), t.getDate()});
+    private void loadDataToTable() {
+        tableModel.setRowCount(0);
+        // Gọi Controller để lấy dữ liệu (đúng chuẩn MVC)
+        for (Transaction t : controller.getAllTransactions()) {
+            tableModel.addRow(new Object[]{t.getId(), t.getAmount(), t.getCategory(), t.getNote(), t.getDate()});
         }
     }
 
     public static void main(String[] args) {
-        new MainFrame();
+        new MainFrame().setVisible(true);
     }
 }
